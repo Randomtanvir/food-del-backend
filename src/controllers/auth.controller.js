f0c62135e7bcb05c8ece7a14f78202c09fc042b8
Tanvir <tanvir.info07@gmail.com>
2026-01-11 21:58:57 +0600

    test

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -103,4 +103,3 @@ export const logout = async (req, res) => {
     });
   }
 };
-//test
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from "express";
 import {
-  register,
-  login,
-  refreshToken,
   logout,
   update,
   checkLoginController,
+  register,
+  login,
 } from "../controllers/auth.controller.js";
 import { protectedRoute } from "../middleware/auth.middleware.js";
 import { uploadSingleImage } from "../middleware/uploadImage.js";
@@ -14,7 +13,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/refresh", refreshToken);
+router.post("/refresh");
 router.get("/logout", logout);
 router.patch("/update", protectedRoute, uploadSingleImage("image"), update);
 router.get("/check-login", protectedRoute, checkLoginController);
